a4b3551b3398304fd3132e925d6c96b943241057
DichanShrestha <blackuniverse.808@gmail.com>
2025-06-29 15:32:22 +0545

    d

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -7,7 +7,7 @@ import { useSearchParams } from "next/navigation";
 export default function Page() {
   const searchParams = useSearchParams();
   const role = searchParams.get("role") || "USER";
-
+  //ask
   return (
     <div className="flex h-screen items-center justify-center">
       <SignIn signUpUrl={`/sign-up?role=${role}`} redirectUrl={`/${role}`} />
--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,6 +1,7 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
+  //sd
   return (
     <div className="flex h-screen items-center justify-center">
       <SignUp signInUrl="/sign-in" redirectUrl={"/admin"} />
